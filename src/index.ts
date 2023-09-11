fe355794afea2c67a5b507b9a1a9c2efde29cb01
Cody Nhan Pham <87593612+codynhanpham@users.noreply.github.com>
2023-09-11 09:41:45 -0500

    add rate limit

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,22 @@ import {Request, Response, NextFunction} from 'express';
 const express = require('express');
 const app = express();
 import fetch, {Response as FetchResponse} from 'node-fetch';
+const { rateLimit } = require('express-rate-limit');
 
 const imageGenRouter = require('./api/color');
 const thumbnailGenRouter = require('./api/thumbnail');
 
 const port = process.env.PORT || 3000; // default port to listen
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 1000, // Limit each IP to this number of requests per `window` (here, per 15 minutes)
+	standardHeaders: 'draft-7',
+	legacyHeaders: false,
+  message: 'You can only make 1000 requests every 15 minutes.',
+});
+
+app.use(limiter);
 
 app.get('/ping', (req: Request, res: Response, next: NextFunction) => {
   return res.status(200).send('pong');
